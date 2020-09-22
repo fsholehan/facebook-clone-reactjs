34bc13881de32bf08e052d5cc89a22c709243e6d
Your Name <you@example.com>
2020-09-22 09:42:57 +0700

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+// PUT your firebase config here
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
---
+++ src/firebase2.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAU9198K8izybcPgEAK2lgg7OJystpLi6w",
+  authDomain: "facebook-bc6aa.firebaseapp.com",
+  databaseURL: "https://facebook-bc6aa.firebaseio.com",
+  projectId: "facebook-bc6aa",
+  storageBucket: "facebook-bc6aa.appspot.com",
+  messagingSenderId: "841932507701",
+  appId: "1:841932507701:web:e3c1bd45d7bf8107387f7c",
+  measurementId: "G-8852DKG1W5"
+};
